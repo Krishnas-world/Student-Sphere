92fb3d15936f8a5c15736d02048183e97716d622
Krishnas-world <krishnapallan128@gmail.com>
2024-04-09 23:07:50 +0530

    Started Build

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <p className="text-3xl font-bold underline bg-red-400 text-center">Hello World</p>
+    </>
+  )
+}
+
+export default App
